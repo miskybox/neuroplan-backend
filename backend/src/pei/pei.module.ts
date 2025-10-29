62921c4ecf9fde8807c2462ee66f1c94a2107a50
SiGu <miskybox@gmail.com>
2025-10-29 12:17:46 +0100

    DELETE-duplicates files, PEI

--- backend/src/pei/pei.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PeiController } from './pei.controller';
-import { RenderModule } from '../render/render.module';
-
-@Module({
-  imports: [RenderModule],
-  controllers: [PeiController],
-})
-export class PeiModule {}
